958fd95a118f7a1f38f33da1a316409d33a16fba
Marco <marcopulv19@gmail.com>
2024-12-15 14:57:15 +0100

    Reordering

--- makefile/main.c
+++ makefile/main.c
@@ -6,7 +6,14 @@
 #define B -1
 
 int main (int argc, char* argv[]) {
+
+    if (argc!=2) {
+        printf("Inserire un parametro per l'esecuzione!\n"); 
+        return 1; }
+    int y = argv[0][0];
+
     int (*f)(int,int) = sum; 
     int x = f(A,B);
     printf("Result = %d\n",x);
+    
     }
